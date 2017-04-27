3a9374afc2bd9eabab42e8d8cc5aea60e3d4ed09
Shruti Rao <shruti.rao@feisystems.com>
2017-04-27 15:48:46 -0400

    Delete OAuth2 maven dependency and references
    #13516:As a developer, I will have integrated try-my-policy API with C2S V3

--- tryPolicy/src/main/java/gov/samhsa/c2s/trypolicy/TryPolicyApplication.java
+++ tryPolicy/src/main/java/gov/samhsa/c2s/trypolicy/TryPolicyApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableResourceServer
 @EnableDiscoveryClient
 @EnableFeignClients
 public class TryPolicyApplication {
