b6d95e66dbcf7f28ea9790581dc680d28f7d8e43
Shruti Rao <shruti.rao@feisystems.com>
2017-05-15 13:47:53 -0400

    Add locale to try-policy
    #13763:As a developer, I will have implemented the Try My Policy UI and integrated with backend [v3.0]

--- tryPolicy/src/main/java/gov/samhsa/c2s/trypolicy/service/TryPolicyService.java
+++ tryPolicy/src/main/java/gov/samhsa/c2s/trypolicy/service/TryPolicyService.java
@@ -2,6 +2,8 @@ package gov.samhsa.c2s.trypolicy.service;
 
 import gov.samhsa.c2s.trypolicy.service.dto.TryPolicyResponse;
 
+import java.util.Locale;
+
 public interface TryPolicyService {
-    TryPolicyResponse getSegmentDocXHTML(String documentId, String consentId, String patientId, String purposeOfUse);
+    TryPolicyResponse getSegmentDocXHTML(String documentId, String consentId, String patientId, String purposeOfUse, Locale locale);
 }
--- tryPolicy/src/main/java/gov/samhsa/c2s/trypolicy/web/TryPolicyController.java
+++ tryPolicy/src/main/java/gov/samhsa/c2s/trypolicy/web/TryPolicyController.java
@@ -3,22 +3,30 @@ package gov.samhsa.c2s.trypolicy.web;
 import gov.samhsa.c2s.trypolicy.service.TryPolicyService;
 import gov.samhsa.c2s.trypolicy.service.dto.TryPolicyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class TryPolicyController {
 
+    private final TryPolicyService tryPolicyService;
+
     @Autowired
-    private TryPolicyService tryPolicyService;
+    public TryPolicyController(TryPolicyService tryPolicyService) {
+        this.tryPolicyService = tryPolicyService;
+    }
 
     @RequestMapping(value = "/tryPolicyXHTML", method = RequestMethod.GET)
     public TryPolicyResponse tryPolicyByConsentIdXHTML(@RequestParam("documentId") String documentId,
                                                        @RequestParam("consentId") String consentId,
                                                        @RequestParam("patientId") String patientId,
-                                                       @RequestParam("purposeOfUseCode") String purposeOfUseCode) {
-        return tryPolicyService.getSegmentDocXHTML(documentId, consentId, patientId, purposeOfUseCode);
+                                                       @RequestParam("purposeOfUseCode") String purposeOfUseCode,
+                                                       @RequestHeader("Accept-Language") Locale locale) {
+        return tryPolicyService.getSegmentDocXHTML(documentId, consentId, patientId, purposeOfUseCode, locale);
     }
 }
